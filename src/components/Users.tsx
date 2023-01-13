3b803c5b375307501ed28578c20406209a10df59
Wal Monfortt <71857487+WallMonfortt@users.noreply.github.com>
2023-01-13 15:16:48 -0600

    render data

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,18 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { reqRes } from '../API/reqRes';
+import { User, UsersResponse } from '../interfaces/reqRes';
 
 const Users = () => {
 
+    const [users, setUsers] = useState<User[]>([]);
+
     useEffect(() => {
       // call Api
-      reqRes.get("/users").then((res) => {
-        console.log(res.data);
+      reqRes.get<UsersResponse>("/users").then((res) => {
+        //console.log(res.data.data);
+        setUsers(res.data.data);
       }
         ).catch((err) => {
             console.log(err);
             }
         );
     }, [])
+
+    const renderUsers = ( {id, first_name, last_name,email,avatar}: User) => {
+        return (
+            <tr key={id.toString()}>
+                <td>{id}</td>
+                <td>{first_name} {last_name}</td>
+                <td>{email}</td>
+                <td>
+                    <img src={avatar}
+                        alt={first_name}
+                        style={{width: 35, borderRadius: 100}}
+                    />
+                </td>
+            </tr>
+        )
+    }
     
   return (
     <>
@@ -23,11 +43,19 @@ const Users = () => {
                 <th>Id</th>
                 <th>Name</th>
                 <th>Email</th>
+                <th>Avatar</th>
             </tr>
         </thead>
         <tbody>
+            {
+                users.map((user) => renderUsers(user))
+            }
         </tbody>
         </table>
+
+        <button className="btn btn-primary">
+            load more
+        </button>
     </>
   )
 }
